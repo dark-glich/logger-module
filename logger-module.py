7abab062d33046cac7c9f70dee875eff594e0045
dark-glich <mohdbilal9808464636@gmail.com>
2020-10-05 23:19:04 +0530

    #
    #

---
+++ logger-module.py
@@ -0,0 +1,19 @@
+import logging
+logger = logging.getLogger(__name__)
+#creating streamhandler and filehandler
+st = logging.StreamHandler()
+fi = logging.FileHandler("handler")
+#setting level
+st.setLevel(logging.WARNING)
+fi.setLevel(logging.ERROR)
+# formating streamhandler and filehandler
+for_st = logging.Formatter('%(levelname)s - %(asctime)s, %(name)s - %(message)s', 
+datefmt='%d/%h/%Y %I:%M:%S')
+st.setFormatter(for_st)
+fi.setFormatter(for_st)
+# adding streamhandler and filehandler
+logger.addHandler(st)
+logger.addHandler(fi)
+# creating error and warning
+logger.warning("this is a warning")
+logger.error("this is a error")
